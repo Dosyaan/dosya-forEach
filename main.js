177de0493c8a5b7b400b913d954df50b7d93f97c
Dosyaan <dastanmatraimov79@gmail.com>
2024-10-28 17:59:01 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// task - 1
+
+// const fruits = ['яблоко', 'банан', 'апельсин']
+// fruits.forEach(item =>{
+//     console.log(item)
+// })
+
+// task - 2
+
+// const num = [1,2,3]
+// num.forEach(item => {
+//     console.log(item*2)
+// })
+
+
+// task - 3
+
+// const sum = [5,10,15]
+// let Sum=0
+// sum.forEach(item => {
+//     console.log(Sum+=item)
+// })
+
+// task - 4
+
+// const text = ["hello", "world"]
+// text.forEach(item => {
+//     console.log(item.toUpperCase())
+// })
+
+
+
+// task - 5
+
+// const num = [2, 5, 8, 1, 10]
+
+// num.forEach(item => {
+//     if(item > 5){
+//         console.log(item)
+//     }
+// })
